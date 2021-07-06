beb05a299cb6948bd333c0b65ae0237e6f4e7f1c
aram17nonymous <arammartirosyann16@gmail.com>
2021-07-06 16:27:38 +0400

    push

--- src/index.js
+++ src/index.js
@@ -1,17 +1,65 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+const api = {
+  key: "c1c68865ac86ffd7f6886b1b61feeb64",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function App() {
+  const [weather, setWeather] = useState({});
+  const [search, setSearch] = useState("");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  const searchCountries = (event) => {
+    if (event.key === "Enter") {
+      fetch(`${api.base}weather?q=${search}&units=metric&APPID=${api.key}`)
+        .then((res) => res.json())
+        .then((result) => {
+          setWeather(result);
+          setSearch("");
+        });
+    }
+  };
+
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date}, ${month} ${year}`;
+  };
+  const inputChange = (e) => {
+    setSearch(e.target.value);
+  };
+  return (
+    <div className={typeof weather.main != "undefined" ? (weather.main.temp > 16 ? "app sun" : "app") : "app"}>
+      <main>
+        <div className="search-box">
+          <input onKeyPress={searchCountries} value={search} onChange={inputChange} type="text" placeholder="Enter country/city" className="search-bar" />
+        </div>
+        {typeof weather.main != "undefined" ? (
+          <div>
+            <div className="country">
+              <div className="country-name">
+                {weather.name}, {weather.sys.country}
+              </div>
+              <div className="date">{dateBuilder(new Date())}</div>
+            </div>
+            <div className="weather-data">
+              <div className="celcius">{Math.round(weather.main.temp)}°C</div>
+              <div className="weather-type">{weather.weather[0].main}</div>
+            </div>
+          </div>
+        ) : (
+          <div></div>
+        )}
+      </main>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
